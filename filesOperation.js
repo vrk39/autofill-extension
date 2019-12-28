575584f0005490990db31d26eacbefcccbf4b24d
vrk39 <kansevinayak1993@gmail.com>
2019-12-28 09:20:53 +0530

    Add methods for store and get file data in memory

--- filesOperation.js
+++ filesOperation.js
@@ -6,36 +6,12 @@ function Upload() {
    //Reference the FileUpload element.
    var fileUpload = document.getElementById("fileUpload");
 
-   //Validate whether File is valid Excel file.
-   var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx)$/;
-   if (regex.test(fileUpload.value.toLowerCase())) {
-      if (typeof(FileReader) != "undefined") {
-         var reader = new FileReader();
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-         //For Browsers other than IE.
-         if (reader.readAsBinaryString) {
-            reader.onload = function(e) {
-               ProcessExcel(e.target.result);
-            };
-            reader.readAsBinaryString(fileUpload.files[0]);
-         } else {
-            //For IE Browser.
-            reader.onload = function(e) {
-               var data = "";
-               var bytes = new Uint8Array(e.target.result);
-               for (var i = 0; i < bytes.byteLength; i++) {
-                  data += String.fromCharCode(bytes[i]);
-               }
-               ProcessExcel(data);
-            };
-            reader.readAsArrayBuffer(fileUpload.files[0]);
-         }
-      } else {
-         alert("This browser does not support HTML5.");
-      }
-   } else {
-      alert("Please upload a valid Excel file.");
-   }
+function getXlsFileData(){
+   return xlsjson_object;
 }
 
 function ProcessExcel(data) {
@@ -86,7 +62,107 @@ function ProcessExcel(data) {
       cell.innerHTML = excelRows[i].Country;
    }
 
-   var dvExcel = document.getElementById("dvExcel");
-   dvExcel.innerHTML = "";
-   dvExcel.appendChild(table);
+var ExcelToJSON = function() {
+
+   this.parseExcel = function(file) {
+     var reader = new FileReader();
+
+     reader.onload = function(e) {
+       var data = e.target.result;
+       var workbook = XLSX.read(data, {
+         type: 'binary'
+       });
+       workbook.SheetNames.forEach(function(sheetName) {
+         // Here is your object
+         var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+         var json_object = JSON.stringify(XL_row_object);
+         //console.log(JSON.parse(json_object));
+         xlsjson_object = json_object;
+         saveFileData(json_object);
+         //return json_object;
+         //jQuery( '#xlx_json' ).val( json_object );
+       })
+     };
+
+     reader.onerror = function(ex) {
+       console.log(ex);
+     };
+
+     reader.readAsBinaryString(file);
+   };
+};
+
+function handleFileSelect(evt) {
+ 
+ var files = evt.target.files; // FileList object
+ setFileName(files[0]);
+
+ var xl2json = new ExcelToJSON();
+ xl2json.parseExcel(files[0]);
+ var fileData = getXlsFileData();
+ console.log('file data recevied printing the data');
+ sleep(3000).then(() => { sendMessages });
+ console.log(fileData);
 }
+
+var feedFileData = function(obj) {
+  saveFileData(obj);
+};
+
+var feedFileName = function(obj) {
+  saveFileData(obj);
+};
+
+function saveFileData(obj) {
+  saveDataInMemory('fileDataInfo', obj);
+}
+
+
+function getFileData(obj) {
+  getDataInMemory('fileDataInfo');
+}
+
+function getFileName() {
+  getDataInMemory('fileName');
+}
+
+
+function setFileName(objFileName) {
+  setDataInMemory('fileName', objFileName);
+}
+
+function saveDataInMemory(key, obj){
+  if (!obj) {
+    console.log('Error: No data provided for saving');
+    return;
+  }
+  chrome.storage.local.set({key : obj},function(){
+      console.log('Data saved in memory');
+  });
+}
+
+function getDataInMemory(key) {
+  var obj;
+  if (!key) {
+    console.log('Error: No key name provided for get data');
+    return;
+  }
+  chrome.storage.local.get(['key'], function(result) {
+    console.log('Value currently is ' + result.key);
+  });
+  sleep(2000);
+  obj = result.key;
+  return obj;
+}
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (var key in changes) {
+    var storageChange = changes[key];
+    console.log('Storage key "%s" in namespace "%s" changed. ' +
+                'Old value was "%s", new value is "%s".',
+                key,
+                namespace,
+                storageChange.oldValue,
+                storageChange.newValue);
+  }
+});
--- popup.js
+++ popup.js
@@ -1,6 +1,12 @@
 $(function(){
-document.getElementById("fill").addEventListener("click", sendMessages);
-document.getElementById('fileUpload').addEventListener('change', Upload, false);
+	document.addEventListener('DOMContentLoaded', function() {
+		getFileInfoAndRestoreInDom();
+		// event listener for the button inside popup window
+		document.getElementById("fill").addEventListener("click", sendMessages);
+		document.getElementById('upload').addEventListener('change', handleFileSelect, false);
+	});
+//document.getElementById("fill").addEventListener("click", sendMessages);
+//document.getElementById('upload').addEventListener('change', handleFileSelect, false);
 });
 
 
@@ -55,212 +61,31 @@ function sendMessages(){
 
 }
 
-function readFile(){
-	var file = this.files;
-	var reader = new FileReader();
-	reader.onload = readSuccess;
-	function readSuccess(evt){
-		var field = document.getElementById('main');
-		field.innerHTML = evt.target.result;
-	}
-	reader.readAsText(file);
-}
-
-function Upload(evt) {
-	//Reference the FileUpload element.
-	//var fileUpload = document.getElementById("fileUpload");
-	const fileUpload = this.files;
+// fetch the URL of the current tab, add inside the window
+function addFiles(evt) {
 	var files = evt.target.files;
-
-	for (var i = 0, len = files.length; i < len; i++) {
-        var file = files[i];
-
-        var reader = new FileReader();
-
-        reader.onload = (function(f) {
-            return function(e) {
-                // Here you can use `e.target.result` or `this.result`
-				// and `f.name`.
-				var field = document.getElementById('main');
-				field.innerHTML = evt.target.result;
-            };
-        })(file);
-
-        reader.readAsText(file);
-    }
-
-	//Validate whether File is valid Excel file.
-	/*var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx)$/;
-	//if (regex.test(fileUpload.value.toLowerCase())) {
-		if (typeof (FileReader) != "undefined") {
-			var reader = new FileReader();
-			
-			//For Browsers other than IE.
-			if (reader.readAsBinaryString) {
-				reader.onload = (function(f) {
-					return function(e) {
-						// Here you can use `e.target.result` or `this.result`
-						// and `f.name`.
-						ProcessExcel(e.target.result);
-					};
-					
-				})(file);
-				reader.readAsBinaryString(fileUpload.files[0]);
-			} else {
-				//For IE Browser.
-				reader.onload = function (e) {
-					var data = "";
-					var bytes = new Uint8Array(e.target.result);
-					for (var i = 0; i < bytes.byteLength; i++) {
-						data += String.fromCharCode(bytes[i]);
-					}
-					ProcessExcel(data);
-				};
-				reader.readAsArrayBuffer(fileUpload.files[0]);
-			}
-		} else {
-			alert("This browser does not support HTML5.");
-		}*/
-	//} else {
-	//	alert("Please upload a valid Excel file.");
-	//}
-}
-function ProcessExcel(data) {
-	//Read the Excel File data.
-	var workbook = XLSX.read(data, {
-		type: 'binary'
-	});
-
-	//Fetch the name of First Sheet.
-	var firstSheet = workbook.SheetNames[0];
-
-	//Read all rows from First Sheet into an JSON array.
-	var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[firstSheet]);
-
-	//Create a HTML Table element.
-	var table = document.createElement("table");
-	table.border = "1";
-
-	//Add the header row.
-	var row = table.insertRow(-1);
-
-	//Add the header cells.
-	var headerCell = document.createElement("TH");
-	headerCell.innerHTML = "Id";
-	row.appendChild(headerCell);
-
-	headerCell = document.createElement("TH");
-	headerCell.innerHTML = "Name";
-	row.appendChild(headerCell);
-
-	headerCell = document.createElement("TH");
-	headerCell.innerHTML = "Country";
-	row.appendChild(headerCell);
-
-	//Add the data rows from Excel file.
-	for (var i = 0; i < excelRows.length; i++) {
-		//Add the data row.
-		var row = table.insertRow(-1);
-
-		//Add the data cells.
-		var cell = row.insertCell(-1);
-		cell.innerHTML = excelRows[i].Id;
-
-		cell = row.insertCell(-1);
-		cell.innerHTML = excelRows[i].Name;
-
-		cell = row.insertCell(-1);
-		cell.innerHTML = excelRows[i].Country;
-	}
-
-	var dvExcel = document.getElementById("dvExcel");
-	dvExcel.innerHTML = "";
-	dvExcel.appendChild(table);
+    var fileName = files[0];
 }
 
-/*
-$(function(){
+function readFileAndSaveData(){
 
-console.log('text changed');
+}
 
-    let params = {
-      active: true,
-      currentWindow: true
-    };
-    chrome.tabs.query(params, gotTabs);
+function getFileInfoAndRestoreInDom(){
+	var filName = getFileName();
+	addFileNameToDom(fileName);
+}
 
-	function gotTab(){
-		console.log('got tabs');
-		console.log(tabs);
+var getProperty = function (propertyName) {
+    return obj[propertyName];
+}
 
-		var data = {
-		"property_type": "Apartment",
-		"is_live": "1",
-		"bathrooms": "3",
-		"year_built": "1998",
-		"bathrooms": "3",
-  		"age_of_roof": "3",
-		"garage": "Detached carpool",
-		"sq_footage": "1000",
-		"stories": "5+",
-		"is_owned": "Owned",
-		"bedrooms": "4",
-		  "additional_coverage": "Contents Coverage",
-		  "coverage_term": "5 Years",
-		  "desired_coverage_amount": "150000",
-		  "desired_deductible": "750",
-		  "desired_liability_coverage": "100,000",
-		  "is_insured": "American Tri-Star Insurance Services",
-		  "credit_rating": "Good",
-		  "is_claim_3_years": "0",
-		  "title": "Mrs",
-		  "blood_group": "A Negative",
-		  "first_name": "Vinayak",
-		  "address": "Andheri",
-		  "last_name": "Kanse",
-		  "zip_code": "400057",
-		  "gender": "1",
-		  "phone": "8551996211",
-		  "emailid": "vinayak@test.com"	};
-	chrome.tabs.sendMessage(tabs[0].id, data);
+function addFileNameToDom(fileName) {
+	if(fileName){
+		if("" != fileName){
+			document.getElementById("fileName").innerHTML = fileName;
+			document.getElementById("myFile").disabled = true;
+		}
 	}
-
-	$('#auto_fill').click(function(){
-	console.log( 'popup js loaded and its wroking');
-	something();
-var data = {
-  "property_type": "Apartment",
-  "is_live": "1",
-  "bathrooms": "3",
-  "year_built": "1998",
-  "bathrooms": "3",
-  "age_of_roof": "3",
-  "garage": "Detached carpool",
-  "sq_footage": "1000",
-  "stories": "5+",
-  "is_owned": "Owned",
-  "bedrooms": "4",
-  "additional_coverage": "Contents Coverage",
-  "coverage_term": "5 Years",
-  "desired_coverage_amount": "150000",
-  "desired_deductible": "750",
-  "desired_liability_coverage": "100,000",
-  "is_insured": "American Tri-Star Insurance Services",
-  "credit_rating": "Good",
-  "is_claim_3_years": "0",
-  "title": "Mrs",
-  "blood_group": "A Negative",
-  "first_name": "Vinayak",
-  "address": "Andheri",
-  "last_name": "Kanse",
-  "zip_code": "400057",
-  "gender": "1",
-  "phone": "8551996211",
-  "emailid": "vinayak@test.com",
-
-};
-fillInsuranceForm(data);
-//	ns.something();
-	});
-
-});*/
+	
+}
